24084c3fa2c9c6b382c45cfeb542bf1a88181950
yowo86po <yowo85po@gmail.com>
2025-04-08 00:21:37 +0800

    yowo86po

--- url.js
+++ url.js
@@ -8,7 +8,6 @@
 
 
 const urls = [
-    'https://forms.gle/FG4AHxfeaV4navwT9',
     'https://forms.gle/AwDmQYQykZmT2jnr9',
     'https://forms.gle/env48rpS3PciUSTD6',
     'https://forms.gle/LKbS7Hz9kpHSg2na7'
