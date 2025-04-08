a76dbe6949a055c6c899ee4c120213a37e0a0bcd
yowo86po <yowo85po@gmail.com>
2025-04-08 14:23:09 +0800

    yowo86po

--- url.js
+++ url.js
@@ -5,9 +5,10 @@
  * 1. 網址請用引號（或稱「撇號」，單引號或雙引號皆可）包起來
  * 2. 包起來的網址之間用逗號分隔
  */
+
 const urls = [
-        'https://forms.gle/FG4AHxfeaV4navwT9',
-        'https://forms.gle/AwDmQYQykZmT2jnr9'
-        'https://forms.gle/env48rpS3PciUSTD6'
+	'https://forms.gle/FG4AHxfeaV4navwT9',
+        'https://forms.gle/AwDmQYQykZmT2jnr9',
+        'https://forms.gle/env48rpS3PciUSTD6',
         'https://forms.gle/LKbS7Hz9kpHSg2na7'
 ];
